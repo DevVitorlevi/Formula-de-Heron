1cee6e093bb4fbcd0295c83b894523ab49079b51
Dev Vitor <160429477+DevVitorlevi@users.noreply.github.com>
2024-08-26 13:55:44 -0300

    att

--- script.js
+++ script.js
@@ -1,13 +1,12 @@
-
-
 document.addEventListener('keydown',e =>{
     if(e.key == 'Escape'){
-    Limpar();}
+    Limpar();
+}
 })
 document.addEventListener('keypress', e=>{
     if(e.key == 'Enter'){
-        Calcular();
-    }
+    Calcular();
+}
 })
 
 function Calcular(){
@@ -16,16 +15,23 @@ function Calcular(){
     let LadoC=parseFloat(document.getElementById('ladoc').value)
     let res =document.querySelector('.res')
     res.innerHTML=''
-    if(!isNaN(LadoA) &&  !isNaN(LadoB) && !isNaN(LadoC)){
+    if(isNaN(LadoA) ||  isNaN(LadoB) || isNaN(LadoC)){
+        res.innerHTML='Falta Dados'
+
+    }else{
+        if(LadoA >0 && LadoB >0 && LadoC >0){
+
         let P = (LadoA+LadoB+LadoC)/2
         let Area =Math.sqrt(P * (P-LadoA) *(P-LadoB) * (P-LadoC))
         res.innerHTML='Calculando:'
         res.innerHTML+=`<br/>O Triangulo com os lados A:${LadoA}; B:${LadoB}; C:${LadoC} Tem sua <strong>Área:${Area.toFixed(2)}</strong>`
 
-    }else{
-        res.innerHTML='Impossivel Calcular'
+    }else if(LadoA<=0|| LadoB<=0|| LadoC<=0){
+        res.innerHTML='Impossivel Calcular <br/> Números Negativos ou Nulos' 
     }
-}
+        }   
+            }
+
 function Limpar(){
     document.getElementById('ladoa').value=''
     document.getElementById('ladob').value=''
